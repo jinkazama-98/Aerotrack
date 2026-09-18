753adfbd799ef2fbe6c4f9ad2b2ece641dec2541
jinkazama-98 <jink8246@gmail.com>
2026-09-18 14:39:43 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,14 @@
 self.addEventListener('install', (e) => {
+    // Forces the waiting service worker to become the active service worker.
     self.skipWaiting();
 });
 
 self.addEventListener('activate', (e) => {
+    // Tells the active service worker to take control of the page immediately.
     e.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', (e) => {
-    // Pass-through network fetching for PWA compliance
+    // Pass-through network fetching for PWA compliance to pass the PWABuilder test.
     e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
